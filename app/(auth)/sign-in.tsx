5b4cebc96ddadeab8afdc4bbf83cafdfb7082c32
Fouzia ifat <fouzia786ifat@gmail.com>
2024-05-27 23:08:43 +0530

    initial commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SIgnIn = () => {
+  return (
+    <View>
+      <Text>SIgnIn</Text>
+    </View>
+  );
+};
+
+export default SIgnIn;
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center ">
+      <Text className="text-3xl font-black">profile</Text>
+    </View>
+  );
+};
+
+export default profile;
